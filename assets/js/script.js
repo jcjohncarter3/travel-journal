2d14e4b67860ec947c0eaeb2d918220906c9716d
JOrullian <jedediahorullian@gmail.com>
2024-07-17 20:07:39 -0500

    added map to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,29 @@ const googleMapsApiFirst = "AIzaSyC4o_3wr-";
 const googleMapsApiSecond = "rxXJXUJeT88KC_Z7QVg6y1nOM";
 const googleMapsApi = `${googleMapsApiFirst}${googleMapsApiSecond}`;
 
-
 // Grab script element in html
 const googleMapsScript = document.getElementById("google-maps-script");
+const modalMapScript = document.getElementById("modal-map-script");
 
 // Add googleMapsApi to script element
-if (googleMapsScript) {
-    const apiKey = googleMapsApi;
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&libraries=places&callback=initMap`;
+// if (googleMapsScript) {
+//     const apiKey = googleMapsApi;
+//     googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&libraries=places&callback=initMap`;
+// }
+
+// Display map in modal
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
+
+initMap();
 
 // Functions to open and close a modal
 function openModal() {
@@ -20,22 +34,3 @@ function openModal() {
 }
 const modalButton = document.querySelector('#modal-button');
 modalButton.addEventListener('click', openModal);
-
-// Function to display map in modal
-const displayModalMap = function () {
-    const modalMapScript = document.getElementById("modal-map-script");
-    const apiKey = googleMapsApi;
-    modalMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&libraries=maps,marker&v=beta`;
-};
-
-  const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApi}&callback=myMap`;
-
-
-// Funtion to showcase googleMap
-function googleMap() {
-    const googleMap = new google.maps.Map(
-      document.getElementById("google-maps-script")
-    );
-}
-
-const service = new google.maps.places.PlacesService(map);
